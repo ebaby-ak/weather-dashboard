407e2c220a7df131077ad11f68a40a94c8b22639
ebaby-ak <166087675+ebaby-ak@users.noreply.github.com>
2024-10-13 16:06:17 -0500

    update apiKey for global access

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
+const apiKey = "e1973810f9a04e7b5c1df04fa43bea43";
+
 $(document).ready(function() {
-    const apiKey = "e1973810f9a04e7b5c1df04fa43bea43";
-
-
-$('#searchBtn').on('click', function(){
+$('#searchBtn').on('click', function() {
     const city = $('#cityInput').val();
+    console.log("City:", city);
+
     if (city) {
         getWeather(city);
     } else {
         alert("Enter a city.");
     }
-  })  
+  });  
 });
 
 function getWeather(city) {
     const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+    console.log("API Url:", apiUrl);
+
     fetch(apiUrl)
     .then(function (response) {
+        console.log("Response:", response);
+
         if (!response.ok) {
             throw new Error('Response error');
         }
@@ -25,6 +30,7 @@ function getWeather(city) {
         displayCurrentWeather(data);
     })
     .catch(function(error) {
+        console.log("Error Fetching:", error);
         alert('Error getting weather');
     });
 }
@@ -32,6 +38,8 @@ function getWeather(city) {
 
 
 function displayCurrentWeather(data) {
+console.log('Displaying Weather:', data);
+
     $('#currentCity').text(`${data.city.name} (${new Date().toLocaleDateString()})`);
     $("#currentTemp").text(`Temp: ${data.list[0].main.temp}°F`);
     $('#currentWind').text(`Wind: ${data.list[0].wind.speed} MPH`);
